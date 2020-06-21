5ed8f8884f8fe967c76fe7fbffdb12c01869e948
netcho <kkotlarski@abv.bg>
2020-06-21 21:53:52 +0300

    Introduce vuelidate for form validation

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import store from '@/store'
 import vuetify from '@/plugins/vuetify'
 import axios from '@/plugins/axios'
 import { i18n } from '@/plugins/i18n'
+import Vuelidate from 'vuelidate'
 import App from '@/App'
 
 Vue.config.productionTip = false;
 
 Vue.use(require('vue-moment'));
+Vue.use(Vuelidate);
 
 let instance = new Vue({
   vuetify,

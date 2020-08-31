304106218f79fedd90c8ee18476ed7fd3d74be76
netcho <kkotlarski@abv.bg>
2020-08-31 16:00:09 +0300

    Fix budget listing
    Signed-off-by: netcho <kkotlarski@abv.bg>

--- src/views/Budgets.vue
+++ src/views/Budgets.vue
@@ -1,11 +1,11 @@
 <template>
     <v-item-group>
         <v-container>
-            <v-row v-for="n in rows" :key="n" class="d-flex justify-between">
-                <v-col v-for="k in calculateColumns(n)" :key="k">
-                    <v-card height="200" width="300" tile @click="goToBudget(budgets[(n*k)-1]._id)">
-                        <v-card-title>{{$t('Week')}} {{ budgets[(n*k)-1]._id }}</v-card-title>
-                        <v-card-text>{{$t('TotalSumWeek')}}: {{ budgets[(n*k)-1].totalSum }}</v-card-text>
+            <v-row v-for="n in rows" :key="n" class="d-flex align-start">
+                <v-col v-for="k in calculateColumns(n)" :key="5* (n-1) + k">
+                    <v-card height="200" width="300" tile @click="goToBudget(budgets[5*(n-1)+k-1]._id)">
+                        <v-card-title>{{$t('Week')}} {{ budgets[5*(n-1)+k-1]._id }}</v-card-title>
+                        <v-card-text>{{$t('TotalSumWeek')}}: {{ budgets[5*(n-1)+k-1].totalSum }}</v-card-text>
                     </v-card>
                 </v-col>
             </v-row>
@@ -28,13 +28,13 @@
         },
         methods: {
             calculateColumns: function (rowNumber) {
-                return math.mod(this.budgets.length, 5) ? math.chain(this.budgets.length).subtract(rowNumber * 5).mod(5).done() : 5;
+                return (rowNumber * 5) < this.budgets.length ? 5 : (rowNumber * 5) - this.budgets.length - 1;
             },
             goToBudget: function(week) {
                 this.$router.push({ name: 'ViewBudget', params: { week: week }});
             }
         },
-        mounted: function () {
+        created: function () {
             this.$http.get('/budgets').
             then((response) => {
                 this.budgets = response.data;
